19f9ab3abb7e0e739c80cf6c9404f9905ebe88ac
Andrew Klymenok <klimenok19686@gmail.com>
2024-05-03 13:48:17 +0300

    Виконав 4 завдання

---
+++ pz_1_13/zavd_4.js
@@ -0,0 +1,12 @@
+function createGreetable(str) {
+    const result = Object.create(createGreetable.prototype);
+    result.str = str;
+    return result;
+}
+
+createGreetable.prototype.greet = function(greeting) {
+    return `${greeting}, ${this.str}!`;
+};
+
+const g = createGreetable('Oleg');
+console.log(g.greet('Hello'));
